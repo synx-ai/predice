545a65ea1ede8400a1c6fe3e1dca4ff9dddbd3c9
Carlos Rivera <carlos@synx.co>
2020-09-18 18:38:48 -0500

    fixed production path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/maia/'
+    : '/',
+  outputDir: 'docs',
   devServer: {
      headers: {
         'X-Frame-Options': 'SAMEORIGIN'
     }
-  },
-  outputDir: 'docs'
+  }
 }
