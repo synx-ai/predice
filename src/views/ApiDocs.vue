70adaf43f25d6af8f0837fb9b96f23229b98a368
Carlos Rivera <carlos@synx.co>
2020-09-22 18:09:50 -0500

    upload docs

--- src/views/ApiDocs.vue
+++ src/views/ApiDocs.vue
@@ -1,43 +1,27 @@
 <template>
   <div class="main-wrapper">
-        <section class="section">
-          <div class="container">
-            <h3 class="is-size-3 has-text-white has-text-weight-bold">RESTful API Backend Documentation</h3>
-            <h5 class="is-size-5 has-text-white has-text-weight-light">Synx Machine Learning Platform</h5>
-            <div class="columns">
-              <div class="column">
-                <div class="dark-border-wrap">
-                  <div class="synx-gray-background">
-                  </div>
-                </div>
+
+    <section class="section">
+      <div class="container synx-panel">
+        <h3 class="is-size-3 has-text-white has-text-weight-bold">Documentación</h3>
+        <h5 class="is-size-5 has-text-white has-text-weight-light">Previsualización de la respuesta de los modelos</h5>
+        <div class="columns">
+          <div class="column">
+            <div class="dark-border-wrap">
+              <div class="synx-gray-background">
               </div>
             </div>
           </div>
-        </section>
-
-        <section class="section">
-          <div class="container">
-
-            <div class="columns">
-              <div class="column">
-                <div id="swagger-ui"></div>
-              </div>
-            </div>
-
-          </div>
-        </section>
-
-        <div class="footer-border-wrap">
-          <footer class="footer synx-dark-background">
-            <div class="content has-text-centered has-text-light">
-              <p>
-                <a href="https://synx.ai" target="_blank"><img src="/synx-logo.svg" class="footer-logo"></a>
-              </p>
-
-            </div>
-          </footer>
         </div>
 
+        <div class="columns">
+          <div class="column">
+            <div id="swagger-ui"></div>
+          </div>
+        </div>
+
+      </div>
+    </section>
 
   </div>
 </template>
@@ -49,7 +33,7 @@ export default {
   name: 'Index',
   mounted: () => {
     SwaggerUI({
-      url: "/specs/openapi-v1.yml",
+      url: "/specs/swagger.yaml",
       dom_id: '#swagger-ui'
     })
   }
